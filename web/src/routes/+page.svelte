a8d6b48d1f9d32cbaa951e82243189e1e179cc7c
Punyaslok <12557052+bitsnorbytes@users.noreply.github.com>
2025-02-20 09:33:56 +0530

    Reun

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,3 +1,6 @@
+<svelte:head>
+    <title>Home</title>
+</svelte:head>
 <div class="w-full h-9/10 flex flex-col items-center justify-center">
     <p class="text-6xl md:text-9xl px-16">
         We help communities streamline their event planning.
--- web/src/routes/pricing/+page.svelte
+++ web/src/routes/pricing/+page.svelte
@@ -1,3 +1,6 @@
+<svelte:head>
+    <title>Pricing</title>
+</svelte:head>
 <div class="w-full h-9/10 flex flex-col items-center justify-center">
     <p class="text-6xl md:text-7xl px-16">
         Our services is free for small communities. Drop us an email for more
