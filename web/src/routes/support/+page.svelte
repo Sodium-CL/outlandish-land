f0832a765cf41ad059be637be3481c2e81515d28
Punyaslok <12557052+bitsnorbytes@users.noreply.github.com>
2025-02-25 17:30:06 +0530

    Contact page updated

--- web/src/routes/support/+page.svelte
+++ web/src/routes/support/+page.svelte
@@ -6,12 +6,84 @@
     <title>Support</title>
 </svelte:head>
 <div class="w-full flex flex-col items-center justify-center">
-    <p class="text-xl md:text-2xl mx-8">
-        For queries and support, reach out to us at <span
-            class="inline-flex items-center rounded-md bg-gray-50 px-2 py-1 font-medium text-gray-600 ring-1 ring-gray-500/10 ring-inset"
-            >support@outlandish.land</span
+    <div class="text-xl mx-8">
+        For queries and support, reach out to us at
+        <ul
+            class="rounded-md bg-gray-50 my-4 px-2 py-1 font-normal text-gray-600 ring-1 ring-gray-500/10 ring-inset"
         >
-    </p>
+            <li class="flex items-center my-4">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    class="size-6"
+                >
+                    <path
+                        fill-rule="evenodd"
+                        d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z"
+                        clip-rule="evenodd"
+                    />
+                </svg>
+
+                <span class="mx-2">Punyasloka Parida</span>
+            </li>
+            <li class="flex items-start my-4">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    class="size-6"
+                >
+                    <path
+                        fill-rule="evenodd"
+                        d="M4.5 2.25a.75.75 0 0 0 0 1.5v16.5h-.75a.75.75 0 0 0 0 1.5h16.5a.75.75 0 0 0 0-1.5h-.75V3.75a.75.75 0 0 0 0-1.5h-15ZM9 6a.75.75 0 0 0 0 1.5h1.5a.75.75 0 0 0 0-1.5H9Zm-.75 3.75A.75.75 0 0 1 9 9h1.5a.75.75 0 0 1 0 1.5H9a.75.75 0 0 1-.75-.75ZM9 12a.75.75 0 0 0 0 1.5h1.5a.75.75 0 0 0 0-1.5H9Zm3.75-5.25A.75.75 0 0 1 13.5 6H15a.75.75 0 0 1 0 1.5h-1.5a.75.75 0 0 1-.75-.75ZM13.5 9a.75.75 0 0 0 0 1.5H15A.75.75 0 0 0 15 9h-1.5Zm-.75 3.75a.75.75 0 0 1 .75-.75H15a.75.75 0 0 1 0 1.5h-1.5a.75.75 0 0 1-.75-.75ZM9 19.5v-2.25a.75.75 0 0 1 .75-.75h4.5a.75.75 0 0 1 .75.75v2.25a.75.75 0 0 1-.75.75h-4.5A.75.75 0 0 1 9 19.5Z"
+                        clip-rule="evenodd"
+                    />
+                </svg>
+
+                <span class="mx-2"
+                    >A22, Oshiwara Sandipani Sikshak CHS Ltd<br />Jogeshwari
+                    West, Mumbai - 400102</span
+                >
+            </li>
+            <li class="flex items-center my-4">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    class="size-6"
+                >
+                    <path
+                        d="M10.5 18.75a.75.75 0 0 0 0 1.5h3a.75.75 0 0 0 0-1.5h-3Z"
+                    />
+                    <path
+                        fill-rule="evenodd"
+                        d="M8.625.75A3.375 3.375 0 0 0 5.25 4.125v15.75a3.375 3.375 0 0 0 3.375 3.375h6.75a3.375 3.375 0 0 0 3.375-3.375V4.125A3.375 3.375 0 0 0 15.375.75h-6.75ZM7.5 4.125C7.5 3.504 8.004 3 8.625 3H9.75v.375c0 .621.504 1.125 1.125 1.125h2.25c.621 0 1.125-.504 1.125-1.125V3h1.125c.621 0 1.125.504 1.125 1.125v15.75c0 .621-.504 1.125-1.125 1.125h-6.75A1.125 1.125 0 0 1 7.5 19.875V4.125Z"
+                        clip-rule="evenodd"
+                    />
+                </svg>
+
+                <span class="mx-2">8433650058</span>
+            </li>
+            <li class="flex items-center my-4">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    class="size-6"
+                >
+                    <path
+                        d="M19.5 22.5a3 3 0 0 0 3-3v-8.174l-6.879 4.022 3.485 1.876a.75.75 0 1 1-.712 1.321l-5.683-3.06a1.5 1.5 0 0 0-1.422 0l-5.683 3.06a.75.75 0 0 1-.712-1.32l3.485-1.877L1.5 11.326V19.5a3 3 0 0 0 3 3h15Z"
+                    />
+                    <path
+                        d="M1.5 9.589v-.745a3 3 0 0 1 1.578-2.642l7.5-4.038a3 3 0 0 1 2.844 0l7.5 4.038A3 3 0 0 1 22.5 8.844v.745l-8.426 4.926-.652-.351a3 3 0 0 0-2.844 0l-.652.351L1.5 9.589Z"
+                    />
+                </svg>
+
+                <span class="mx-2">support@outlandish.land</span>
+            </li>
+        </ul>
+    </div>
     <span class="m-10">
         <Icon width="3em" height="3em" fill="oklch(0.637 0.237 25.331)" />
     </span>
