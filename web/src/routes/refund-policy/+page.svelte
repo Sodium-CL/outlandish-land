cbd567fbb206b1d5bd54acc907e09dbe6f191a23
Punyaslok <12557052+bitsnorbytes@users.noreply.github.com>
2025-02-26 11:25:45 +0530

    Refund policy

--- web/src/routes/refund-policy/+page.svelte
+++ web/src/routes/refund-policy/+page.svelte
@@ -9,32 +9,25 @@
     <h2 class="text-2xl font-semibold text-red-600 m-4">
         Refund and Cancellation policy
     </h2>
-    <p class="text-sm mx-8">
+    <div class="text-sm mx-8">
         This refund and cancellation policy outlines how you can cancel or seek
         a refund for a product / service that you have purchased through the
-        Platform. Under this policy: Cancellations will only be considered if
-        the request is made 30 days of placing the order. However, cancellation
-        requests may not be entertained if the orders have been communicated to
-        such sellers / merchant(s) listed on the Platform and they have
-        initiated the process of shipping them, or the product is out for
-        delivery. In such an event, you may choose to reject the product at the
-        doorstep. Outlandish Land does not accept cancellation requests for
-        perishable items like flowers, eatables, etc. However, the refund /
-        replacement can be made if the user establishes that the quality of the
-        product delivered is not good. In case of receipt of damaged or
-        defective items, please report to our customer service team. The request
-        would be entertained once the seller/ merchant listed on the Platform,
-        has checked and determined the same at its own end. This should be
-        reported within 30 days of receipt of products. In case you feel that
-        the product received is not as shown on the site or as per your
-        expectations, you must bring it to the notice of our customer service
-        within 30 days of receiving the product. The customer service team after
-        looking into your complaint will take an appropriate decision. In case
-        of complaints regarding the products that come with a warranty from the
-        manufacturers, please refer the issue to them. In case of any refunds
-        approved by Outlandish Land, it will take 15 days for the refund to be
-        processed to you.
-    </p>
+        Platform. Under this policy:
+        <ul class="list-decimal text-sm mx-8">
+            <li>
+                Cancellations will only be considered if the request is made 30
+                days of placing the order / buying the ticket.
+            </li>
+            <li>
+                The request would be entertained only if the seller/ merchant
+                listed on the platform offers refund on Cancellation.
+            </li>
+            <li>
+                In case of any refunds approved by Outlandish Land, it will take
+                15 days for the refund to be processed to you.
+            </li>
+        </ul>
+    </div>
     <span class="m-10">
         <Icon width="3em" height="3em" fill="oklch(0.637 0.237 25.331)" />
     </span>
