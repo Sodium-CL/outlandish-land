93fc0939b8d618599da2452a3bda56805fd81bf9
Punyasloka Parida <punyasloka.parida@seba.swiss>
2025-03-03 16:08:22 +0530

    Adding View

--- web/src/lib/server/sanity.js
+++ web/src/lib/server/sanity.js
@@ -1,14 +1,15 @@
 
 import {createClient} from '@sanity/client'
 import {env} from '$env/dynamic/private';
-const client = createClient({
-    projectId: env.PROJECT_ID,
-    dataset: env.DATASET,
-    useCdn: true,
-    apiVersion: env.API_VERSION,
-    token: env.SANITY_READ_TOKEN
-})
+
 export async function fetchEventDetails()  {
+    const client = createClient({
+        projectId: env.PROJECT_ID,
+        dataset: env.DATASET,
+        useCdn: true,
+        apiVersion: env.API_VERSION,
+        token: env.SANITY_READ_TOKEN
+    })
     await client.fetch('*[_type == "event"]')
 }
 
