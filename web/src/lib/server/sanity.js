0cbc92bd9c66cce42f5aaad54107e8200da1c0e8
Punyasloka Parida <punyasloka.parida@seba.swiss>
2025-03-03 14:52:21 +0530

    Adding CMS

--- web/src/lib/server/sanity.js
+++ web/src/lib/server/sanity.js
@@ -1,6 +1,6 @@
 
 import {createClient} from '@sanity/client'
-import { PROJECT_ID,DATASET,API_VERSION,SANITY_READ_TOKEN } from '$env/static/private';
+import { PROJECT_ID,DATASET,API_VERSION,SANITY_READ_TOKEN } from '$env/dynamic/private';
 export const client = createClient({
     projectId:PROJECT_ID,
     dataset: DATASET,
